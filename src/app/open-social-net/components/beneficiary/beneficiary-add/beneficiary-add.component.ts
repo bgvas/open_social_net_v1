71f0c2ab63e9e3eb1ba168b654830febe7ff6299
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-12-10 23:28:54 +0200

    Feat: Open_social_net components

--- src/app/open-social-net/components/beneficiary/beneficiary-add/beneficiary-add.component.ts
+++ src/app/open-social-net/components/beneficiary/beneficiary-add/beneficiary-add.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import Stepper from "bs-stepper";
 
 @Component({
   selector: 'app-beneficiary-add',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeneficiaryAddComponent implements OnInit {
 
-  constructor() { }
+  sexOptions: ['Ανδρας', 'Γυναίκα', 'Αλλο'];
+  beneficiaryForm: FormGroup;
+  private wizardStepper: Stepper;
+  date = new Date();
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
+
+    this.initializeForm();
+    this.wizardStepper = new Stepper(document.querySelector('#stepper2'), {
+      linear: false,
+      animation: true
+    });
   }
+
+  initializeForm(): void {
+    this.beneficiaryForm = this.fb.group({
+      'afm': this.fb.control('', [Validators.minLength(9), Validators.maxLength(9)]),
+      'amka': this.fb.control('', [Validators.required]),
+      'adtorpassport': this.fb.control('', [Validators.required, Validators.maxLength(20)]),
+      'nationality': this.fb.control('', [Validators.required]),
+      'firstname': this.fb.control('', Validators.required),
+      'lastname': this.fb.control('', Validators.required),
+      'fartherlastname': this.fb.control('', Validators.required),
+      'fatherfirstname': this.fb.control('', Validators.required),
+      'motherfirstname': this.fb.control('', Validators.required),
+      'birthyear': this.fb.control('', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]),
+      'sex': this.fb.control('', Validators.required),
+      'address': this.fb.control('', Validators.required),
+      'city': this.fb.control('', Validators.required),
+      'zipcode': this.fb.control('', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]),
+      'phone1': this.fb.control('', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+      'phone2': this.fb.control('', [Validators.minLength(10), Validators.maxLength(10)]),
+      'enabled': this.fb.control('', Validators.required)
+    })
+  }
+
+  onSubmit() {
+    if (this.beneficiaryForm.invalid) {
+      this.beneficiaryForm.markAllAsTouched();
+      return;
+    }
+  }
+
+  wizardNext() {
+    this.wizardStepper.next();
+  }
+
+  wizardPrevious() {
+    this.wizardStepper.previous();
+  }
+
+  years(): number[] {
+    const yearsArray = [];
+    for(let i = this.date.getFullYear(); i >= this.date.getFullYear() - 100; i--) {
+      yearsArray.push(i);
+    }
+
+    return yearsArray;
+  }
+
+
 
 }
