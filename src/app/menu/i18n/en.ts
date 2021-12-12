f56735dab7858a652dfbf6949f68d78d36d4123a
Vasileios Georgoulas <b_gvas@yahoo.gr>
2021-12-12 22:40:25 +0200

    other files

---
+++ src/app/menu/i18n/en.ts
@@ -0,0 +1,43 @@
+export const locale = {
+  lang: 'en',
+  data: {
+    MENU: {
+      DASHBOARD: 'Κεντρικό μενού',
+      APP: {
+        SECTION: 'Περιεχόμενα',
+        BENEFICIARY: {
+          COLLAPSIBLE: 'Ωφελούμενοι',
+          REGISTER: {
+            LIST: 'Λίστα',
+            ADD: 'Προσθήκη',
+            PREVIEW: 'Προβολή'
+          }
+        },
+        BENEFACTOR: {
+          COLLAPSIBLE: 'Δωρητές',
+          REGISTER: {
+            LIST: 'Λίστα',
+            ADD: 'Προσθήκη',
+            PREVIEW: 'Προβολή'
+          }
+        },
+        CONTRIBUTOR: {
+          COLLAPSIBLE: 'Χορηγοί',
+          REGISTER: {
+            LIST: 'Λίστα',
+            ADD: 'Προσθήκη',
+            PREVIEW: 'Προβολή'
+          }
+        },
+        SUPPLIER: {
+          COLLAPSIBLE: 'Προμηθευτές',
+          REGISTER: {
+            LIST: 'Λίστα',
+            ADD: 'Προσθήκη',
+            PREVIEW: 'Προβολή'
+          }
+        }
+      }
+    }
+  }
+};
